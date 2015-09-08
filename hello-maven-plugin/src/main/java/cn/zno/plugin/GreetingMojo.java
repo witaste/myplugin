97e11949813c6e1a3f314a0105d0b945ea3449c4
热线-郑兴广 <witaste@qq.com>
2015-09-08 13:24:25 +0800

    初版

---
+++ hello-maven-plugin/src/main/java/cn/zno/plugin/GreetingMojo.java
@@ -0,0 +1,21 @@
+package cn.zno.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Says "Hi" to the user.
+ *
+ */
+@Mojo(name = "sayhi")
+public class GreetingMojo extends AbstractMojo {
+	
+	@Parameter( property = "sayhi.greeting", defaultValue = "Hello World!" )
+    private String greeting;
+	
+	public void execute() throws MojoExecutionException {
+		getLog().info(greeting);
+	}
+}
